d23b92f602324bcda25c9ddf40d81a1a39b1a3bb
maylme <mesnier@mayl.me>
2016-10-31 21:17:56 +0100

    camera qui tourne et decor moche

---
+++ TIM/Assets/AccelerometerInput.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AccelerometerInput : MonoBehaviour
+{
+    void Update()
+    {
+        // transform.Translate(Input.acceleration.x, 0, -Input.acceleration.z);
+        // Vector3 actual_position = transform.position;
+        // transform.Rotate(transform.position * (-(Input.acceleration.x)));
+        //  transform.Rotate(transform.position * Input.acceleration.y);
+        // transform.Rotate(transform.position * Time.deltaTime * Input.acceleration.z);
+
+        Vector3 dir1 = Vector3.zero;
+        Vector3 dir2 = Vector3.zero;
+
+
+        dir1.x = Input.acceleration.y;
+        //dir2.z = Input.acceleration.x;
+         dir2.y = Input.acceleration.x;
+
+        transform.Rotate(dir1);
+        transform.Rotate(dir2);
+    }
+}
