4a54d25f05afef78d35d0cb6a972d1b1aecd9f90
NicolasForget <forget@polytech.unice.fr>
2016-11-01 16:11:48 +0100

    interaction 2

---
+++ TIM/Assets/AccelerometerInt2.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AccelerometerInt2 : MonoBehaviour
+{
+	void Update()
+	{
+		float move = Input.acceleration.y + 0.6f;
+		transform.Translate(0, 0, move);
+	}
+}
---
+++ TIM/Assets/TouchInt2.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class TouchInt2 : MonoBehaviour
+{
+	void Update()
+	{
+		
+		if (Input.touchCount > 0)
+		{
+			Touch myTouch = Input.GetTouch(0);
+			Vector3 dir1 = Vector3.zero;
+			float screenWidth = Screen.width;
+			dir1.y = (myTouch.position.x - (screenWidth / 2)) * 0.002f;
+			transform.Rotate(dir1);
+		}
+
+	}
+}
